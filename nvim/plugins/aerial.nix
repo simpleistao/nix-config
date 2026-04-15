537e2c2018de5a0a3b0263369735fb5073d9905a
simpleistao <simpleistao@gmail.com>
2026-04-14 22:46:56 -0400

    switch to aeriel

---
+++ nvim/plugins/aerial.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+{
+  programs.nixvim.plugins.aerial = {
+    enable = true;
+    settings = {
+      # Use both LSP and Treesitter as backends
+      backends = [ "lsp" "treesitter" ];
+      # Show the symbol tree on the right
+      layout = {
+        min_width = 30;
+        default_direction = "right";
+      };
+      # Close aerial when it's the last window open
+      close_on_select = false;
+    };
+  };
+
+  programs.nixvim.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>a";
+      action = "<cmd>AerialToggle!<CR>";
+      options = { desc = "Toggle Aerial (Outline)"; };
+    }
+    {
+      mode = "n";
+      key = "{";
+      action = "<cmd>AerialPrev<CR>";
+      options = { desc = "Prev Symbol"; };
+    }
+    {
+      mode = "n";
+      key = "}";
+      action = "<cmd>AerialNext<CR>";
+      options = { desc = "Next Symbol"; };
+    }
+  ];
+}
